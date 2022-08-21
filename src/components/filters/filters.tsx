9b589710f2d466f7257402e297a032fa0ec324c8
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-08-21 17:38:08 +0300

    Создал компонент фильтрации товаров

---
+++ src/components/filters/filters.tsx
@@ -0,0 +1,10 @@
+import { ReactFC } from '@src/interfaces/react'
+import React from 'react'
+
+import './filters.scss'
+
+const Filters: ReactFC = () => {
+  return <div className='' />
+}
+
+export default Filters
