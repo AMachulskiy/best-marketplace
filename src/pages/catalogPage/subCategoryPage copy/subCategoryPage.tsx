6371ac4e7962c6d46769743cacf64e01583a3b24
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-08-21 18:13:14 +0300

    Добавил панель сортировки

--- src/pages/catalogPage/subCategoryPage copy/subCategoryPage.tsx
+++ src/pages/catalogPage/subCategoryPage copy/subCategoryPage.tsx
@@ -38,7 +38,23 @@ const SubCategoryPage: ReactFC = () => {
 
   return (
     <>
-      <div className='sub-category-page__sort-panel'>Панель сортировки</div>
+      <div className='sort'>
+        <div className='sort__actions'>
+          <span className='sort__actions-label'>Сортировка по:</span>
+          <div className='sort__action active'>Популярности</div>
+          <div className='sort__action'>Рейтингу</div>
+          <div className='sort__action'>
+            <span>Цене</span>
+            <i className='ic_left-arrow' />
+          </div>
+          <div className='sort__action'>Скидке</div>
+          <div className='sort__action'>Обновлению</div>
+        </div>
+        <div className='sort__view'>
+          <i className='ic_view-2 active' />
+          <i className='ic_view-1' />
+        </div>
+      </div>
       <div className='sub-category-page__products'>{renderProducts(16)}</div>
       <div>Навигация</div>
     </>
