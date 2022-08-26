f5ea827f3b6113d011c211d76e9e5e54d8b87c31
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-08-26 12:32:05 +0300

    Изменил названия переменных в соответствии с интерфейсом

--- src/pages/catalogPage/catalogPage.tsx
+++ src/pages/catalogPage/catalogPage.tsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import { ReactFC } from '@src/interfaces/react'
 import { useParams } from 'react-router-dom'
+import breadcrumbsHelpers from '@src/helpers/breadcrumbsHelpers'
 import Breadcrumbs from '@src/components/breadcrumbs/breadcrumbs'
 import Filters from '@src/components/filters/filters'
 import CategoryList from '@src/components/categoryList/categoryList'
+import ICategoriesParams from '@src/interfaces/params'
 import MainCategoryPage from './mainCategoryPage/mainCategoryPage'
 import SubCategoryPage from './subCategoryPage copy/subCategoryPage'
 
 import './catalogPage.scss'
 
 const CatalogPage: ReactFC = () => {
-  const { subcategory } = useParams()
+  const params: ICategoriesParams = useParams()
+  const breadcrumbsData =
+    breadcrumbsHelpers.getCategoriesBreadcrumbsData(params)
+
   return (
     <div className='wrapper'>
-      <Breadcrumbs />
+      {breadcrumbsData && <Breadcrumbs data={breadcrumbsData} />}
       <h1>Каталог товаров</h1>
       <main className='products'>
         <div className='main-category-page__sidebar'>
-          {!subcategory && <CategoryList />}
-          {subcategory && <Filters />}
+          {!params.subCategory && <CategoryList />}
+          {params.subCategory && <Filters />}
         </div>
         <div className='main-category-page__content'>
-          {!subcategory && <MainCategoryPage />}
-          {subcategory && <SubCategoryPage />}
+          {!params.subCategory && <MainCategoryPage />}
+          {params.subCategory && <SubCategoryPage />}
         </div>
       </main>
     </div>
