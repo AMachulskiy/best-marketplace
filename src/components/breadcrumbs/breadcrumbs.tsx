6b48d0180c22deb4b6c6328ab5dd53ef9b5fc37d
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-08-26 12:30:19 +0300

    Произвел рефакторинг компонента хлебных крошек
    1. Выделил функционал в отдельный файл
    2. Переписал сам компонент

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,81 +1,34 @@
-import categories from '@src/data/menuCategoriesData'
-import { IMenuCategory } from '@src/interfaces/menuCategories'
+import IBreadcrumb from '@src/interfaces/breadcrumb'
 import { ReactFC } from '@src/interfaces/react'
 import routing from '@src/routes/routes'
-import React, { Fragment, ReactNode } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './breadcrumbs.scss'
 
-interface ICategoriesParams {
-  category?: string
-  subcategory?: string
-  innerSubcategory?: string
+interface IBreadcrumbsProps {
+  data: IBreadcrumb[]
 }
 
-interface IOptions {
-  categories: IMenuCategory[]
-  path: string
-  html: ReactNode[]
-  params: string[]
-  i: number
-}
-
-const Breadcrumbs: ReactFC = () => {
-  const params: ICategoriesParams = useParams()
-
-  const renderLinks = (
-    categoriesArr: IMenuCategory[],
-    paramsObj: ICategoriesParams
-  ) => {
-    const { category, subcategory, innerSubcategory } = paramsObj
-    const paramsArr = [category, subcategory, innerSubcategory]
-    const options: IOptions = {
-      categories: categoriesArr,
-      html: [],
-      params: paramsArr,
-      path: '/catalog',
-      i: 0,
-    }
-
-    const getCategory = (data: IOptions): IMenuCategory => {
-      for (let i = 0; i < data.categories.length; i++) {
-        if (data.categories[i].link === data.path) {
-          const { link, title } = data.categories[i]
-          data.html.push(
-            <Fragment key={link}>
-              <span>/</span>
-              {data.params[data.i + 1] ? (
-                <Link to={link}>{title}</Link>
-              ) : (
-                <div>{title}</div>
-              )}
-            </Fragment>
-          )
-          return data.categories[i]
-        }
-      }
-      return null
-    }
-
-    const buildBreadcrumbs = (data: IOptions) => {
-      const { i } = data
-      data.path += `/${data.params[i]}`
-      const matchCategory = getCategory(data)
-      if (data.params[i + 1] && matchCategory.subCategories) {
-        data.i += 1
-        data.categories = matchCategory.subCategories
-        buildBreadcrumbs(data)
-      }
-    }
-    buildBreadcrumbs(options)
-    return options.html
+const Breadcrumbs: ReactFC<IBreadcrumbsProps> = ({ data }) => {
+  const renderLinks = (breadcrumbs) => {
+    const html = breadcrumbs.map(({ label, link }: IBreadcrumb, i) => {
+      const isLast = i + 1 === breadcrumbs.length
+      return (
+        <>
+          <span>/</span>
+          {!isLast && <Link to={link}>{label}</Link>}
+          {isLast && <div>{label}</div>}
+        </>
+      )
+    })
+    return html
   }
 
   return (
     <div className='breadcrumbs'>
       <Link to={routing.home}>Главная</Link>
-      {renderLinks(categories, params)}
+      {renderLinks(data)}
     </div>
   )
 }
