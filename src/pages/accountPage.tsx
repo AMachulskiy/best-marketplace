b0350ee55c3d9dc6de6a5540f3f7413eefc672ed
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-09-20 09:12:53 +0300

    Добавил компонент страницы личного кабинета

---
+++ src/pages/accountPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ReactFC } from '@src/interfaces/react'
+import Account from '@src/modules/account/account'
+
+const AccountPage: ReactFC = () => {
+  return <Account />
+}
+
+export default AccountPage
