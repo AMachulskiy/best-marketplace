d0643804fcbdad393b4cfc317800d6117abd6930
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-10-10 09:18:21 +0300

    Добавил интерфейсы для фильтров товаров

--- src/interfaces/filters.ts
+++ src/interfaces/filters.ts
@@ -1,3 +1,5 @@
+import { ColorsEnum } from './product'
+
 enum orderFilterTypeEnum {
   all,
   buy,
@@ -7,3 +9,30 @@ enum orderFilterTypeEnum {
 }
 
 export default orderFilterTypeEnum
+
+export interface IRange {
+  from: number
+  to: number
+}
+
+export interface ICheckRadio {
+  id: number
+  label: string
+  value: string | number
+}
+
+export type IColor = keyof typeof ColorsEnum
+
+export interface IProductsFilters {
+  colors: IColor[]
+  price: IRange
+  sale: number[]
+  brands: ICheckRadio[]
+}
+
+export interface IProductsSelectedFilters {
+  colors: IColor[]
+  price: IRange
+  sale: number
+  brands: ICheckRadio[]
+}
