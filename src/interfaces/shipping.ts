e890365e88865a6dfd67de00b38fb3cf33b3be8a
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-10-27 13:25:12 +0300

    Добавил два интерфейса для вида платежа и доставки

---
+++ src/interfaces/shipping.ts
@@ -0,0 +1,11 @@
+enum ShippingTypeEnum {
+  postomat = 'postomat',
+  courier = 'courier',
+}
+
+export default ShippingTypeEnum
+
+export enum ShippingTypeValueEnum {
+  postomat = 'Пункт выдачи',
+  courier = 'Курьер',
+}
