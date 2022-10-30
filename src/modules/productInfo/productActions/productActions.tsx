465da814d670867da9d4d1c405cf30024141d004
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-10-30 19:15:18 +0300

    Осуществил вывод данных из хранилища
    + функции добавления в корзину и избранного

--- src/modules/productInfo/productActions/productActions.tsx
+++ src/modules/productInfo/productActions/productActions.tsx
@@ -1,39 +1,78 @@
 import Button from '@src/components/button/button'
 import functionHelpers from '@src/helpers/functionHelpers'
+import priceHelpers from '@src/helpers/priceHelpers'
+import productHelpers from '@src/helpers/productHelpers'
+import { useAppDispatch, useAppSelector } from '@src/hooks/redux'
 import { ReactFC } from '@src/interfaces/react'
-import React, { useState } from 'react'
+import {
+  addToBasket,
+  addToFavorite,
+  deleteFromFavorite,
+} from '@src/store/userStore/userStore'
+import routing from '@src/routes/routes'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import moment from 'moment'
 
 import './productActions.scss'
 
 const ProductActions: ReactFC = () => {
-  const [isFavorite, setIsFavorite] = useState(false)
-  const [inCart, setInCart] = useState(false)
+  const dispatch = useAppDispatch()
+  const {
+    products: { product },
+    user: { basket, favorite },
+  } = useAppSelector((state) => state)
+  const navigate = useNavigate()
+  const priceWithSale = priceHelpers.getSalePrice(product.price, product.sale)
+  const haveProductInBasket = productHelpers.haveProduct(product.id, basket)
+  const haveProductInFavorite = productHelpers.haveProduct(product.id, favorite)
+  const shippingDate = moment().add(product.shipTime, 'day').format('D MMMM')
+
+  const handleFavorite = () => {
+    if (haveProductInFavorite) {
+      dispatch(deleteFromFavorite(product.id))
+    } else {
+      dispatch(addToFavorite(product))
+    }
+  }
+  const buyNow = () => {
+    if (!haveProductInBasket) {
+      dispatch(addToBasket(product))
+    }
+    navigate(routing.basket)
+  }
 
   return (
     <div className='product-actions'>
       <div className='product-actions__top'>
         <div className='product-actions__price'>
-          <strong>{functionHelpers.getDigitNumber(194993)} ₽</strong>
-          <span>{functionHelpers.getDigitNumber(299990)} ₽</span>
+          <strong>{functionHelpers.getDigitNumber(priceWithSale)} ₽</strong>
+          <span>{functionHelpers.getDigitNumber(product.price)} ₽</span>
         </div>
         <i
-          className={`ic_heart ${isFavorite ? 'ic_like-2' : 'ic_heart'}`}
-          onClick={() => setIsFavorite(!isFavorite)}
+          className={`ic_heart ${
+            haveProductInFavorite ? 'ic_like-2' : 'ic_heart'
+          }`}
+          onClick={handleFavorite}
         />
       </div>
       <div className='product-actions__credit'>
         <span>В кредит от</span>
         <strong>{functionHelpers.getDigitNumber(19499)} ₽</strong>
       </div>
-      <Button onClick={() => setInCart(!inCart)}>
-        {inCart ? 'Добавлено в корзину' : 'Добавить в корзину'}
+      <Button onClick={() => dispatch(addToBasket(product))}>
+        {haveProductInBasket ? 'Добавлено в корзину' : 'Добавить в корзину'}
       </Button>
-      <Button theme='outline' onClick={() => alert('Спасибо за покупку!')}>
+      <Button theme='outline' onClick={buyNow}>
         Купить сейчас
       </Button>
       <div className='product-actions__ship'>
-        <strong>31 августа-2 сентября</strong>
+        <strong>{shippingDate}</strong>
         <span>доставит Wildberries со склада Коледино WB</span>
+        <br />
+        <span>
+          Продавец: <strong>{product.seller}</strong>
+        </span>
       </div>
     </div>
   )
