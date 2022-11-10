1769287e127a96a0b096260fd8374985855fc3f6
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-11-10 10:41:03 +0300

    Обновил начальные данные, добавил вывод информации из локальной БД

--- src/modules/account/account.tsx
+++ src/modules/account/account.tsx
@@ -1,8 +1,9 @@
 import { ReactFC } from '@src/interfaces/react'
 import routing from '@src/routes/routes'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '@src/hooks/redux'
-import { changeNotificationStatus } from '@src/store/userStore/userStore'
+import { getUser, setNotificationStatus } from '@src/store/userStore/actions'
+import Preloader from '@src/components/preloader/preloader'
 import AccountCard from './accountCard/accountCard'
 
 import './account.scss'
@@ -11,10 +12,21 @@ const Account: ReactFC = () => {
   const dispatch = useAppDispatch()
   const {
     data: { name, lastname, phone },
+    id,
     isNotification,
     bayed,
     favorite,
+    isLoading,
+    haveData,
   } = useAppSelector((state) => state.user)
+
+  useEffect(() => {
+    if (!haveData) {
+      dispatch(getUser(1))
+    }
+  }, [haveData])
+
+  if (isLoading || !haveData) return <Preloader />
 
   return (
     <div className='account'>
@@ -25,7 +37,14 @@ const Account: ReactFC = () => {
           subTitle='Подтвердить аккаунт'
           label='Телефон'
           text={phone}
-          onNotification={() => dispatch(changeNotificationStatus())}
+          onNotification={() =>
+            dispatch(
+              setNotificationStatus({
+                id,
+                status: !isNotification,
+              })
+            )
+          }
           isNotification={isNotification}
           logout
           isBig
